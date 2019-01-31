194188b60f9d608daff4d84c6ed55935743c83de
Subramanian Ganesan <subash.6471@gmail.com>
2019-01-31 07:32:53 +0530

    Project Setup completed successfully.

---
+++ shoppingbackend/src/main/java/net/vlgs/shoppingbackend/App.java
@@ -0,0 +1,13 @@
+package net.vlgs.shoppingbackend;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
